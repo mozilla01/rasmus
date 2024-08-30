23fe3f647a5ebc0e4fae114db5653bc6ccd1347c
mozilla01 <moizrocks12345@gmail.com>
2024-08-30 08:03:11 +0530

    local api

--- app.js
+++ app.js
@@ -47,40 +47,15 @@ app.post("/interactions", async function (req, res) {
 
     // "write" command
     if (name === "write") {
-      let server_data = [];
-      server_data = JSON.parse(readFileSync("server_data.json", "utf8"));
-      let server_channel = {};
-      if (server_data) {
-        [server_channel] = server_data.filter(
-          (obj) => obj.guild_id == guild_id && obj.channel_id == channel_id,
-        );
-      }
-
       let response = await DiscordRequest(
         "channels/" +
           channel_id +
-          `/messages?limit=100${
-            server_channel?.last_message
-              ? "&after=" + server_channel.last_message
-              : ""
-          }`,
+          `/messages?limit=100`,
       );
       let data = await response.json();
       console.log(data);
       const last_message = data[data.length - 1]?.id;
       console.log("Acknowledging the command");
-      // if (data.length < 50 || !last_message) {
-        if (1 == 2) {
-        // Send a message into the channel where command was triggered from
-        res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content:
-              "There haven't been many messages since the last chronicle. Rasmus needs more tea to write another one.",
-          },
-        });
-        return;
-      } else {
         res.send({
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
@@ -89,23 +64,27 @@ app.post("/interactions", async function (req, res) {
         });
         let prompt =
           "You are Rasmus Ravioli, a Chronicler. Generate a funny, silly chronicle of this conversation on Discord. You are allowed to take silly, humorous jabs or make taunts at people. If you are not sure about the context, you are allowed to assume or make things up. In between conversations, if you encounter your previous chronicles, you can reference characters or statements in the old chronicle in your new chronicle you are currently writing, only if there is a clear relation between the two. Everyone reads your chronicles but pretend as though no one reads them.";
+          let messages = "";
         for (const message of data.slice().reverse()) {
           if (message.content) {
-            prompt += `\n${message.author.username}: ${message.content}`;
+            messages += `${message.author.username}: ${message.content}\n`;
           }
         }
 
         try {
-          response = await fetch("http://localhost:11434/api/generate", {
+          response = await fetch("http://localhost:8080/chat/completions", {
             method: "POST",
+            headers: {
+              "Content-type": "application/json"
+            },
             body: JSON.stringify({
               model: "phi3",
-              prompt: prompt,
-              stream: false,
+              messages: [{role: "system", content: prompt}, {role: "user", content: messages}],
             }),
           });
           data = await response.json();
-          let generated_response = data.response;
+          let generated_response = data.choices[0].message.content;
+          console.log(generated_response);
           let start = 0,
             end = Math.min(2000, generated_response.length);
 
@@ -124,20 +103,6 @@ app.post("/interactions", async function (req, res) {
             sleep(300);
           }
           console.log("Chronicle sent");
-          let obj = [
-            ...server_data.filter((obj) => obj !== server_channel),
-            {
-              guild_id: guild_id,
-              channel_id: channel_id,
-              last_message: last_message,
-            },
-          ];
-          writeFile("server_data.json", JSON.stringify(obj), (err) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-          });
         } catch (err) {
           console.error(err);
           response = await DiscordRequest(`channels/${channel_id}/messages`, {
@@ -148,7 +113,6 @@ app.post("/interactions", async function (req, res) {
             },
           });
         }
-      }
     }
   }
 });
